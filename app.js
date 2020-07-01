db68fe3172d2ac0e0c7298b6324b266b59dd3264
Arunasva (Ash) <43392585+ArunasvaArunasva@users.noreply.github.com>
2020-06-30 21:43:29 -0400

    Adding All The Files

--- app.js
+++ app.js
@@ -1,85 +1,21 @@
-/* CREATE SERVER
-var fs = require('fs')
+// Entry point for the application
 
-var server = http.createServer(function (req,res)
-{
-      console.log('request made ' + req.url);
-      res.writeHead(200,{'Content-Type':'text/plain'});
-      res.end('fuck you');
-});
-server.listen(3000,'127.0.0.1');
-console.log('listening to 3000 motherfucker');
-
-//PIPING USING SERVER
-var http = require('http');
-var fs = require('fs');
-var server = http.createServer(function (req,res)
-{
-    console.log('request made ' + req.url);
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    var myReadStream = fs.createReadStream(__dirname + '/ReadMe.txt','utf8');
-    myReadStream.pipe(res);
-});
-server.listen(3000,'127.0.0.1');
-console.log('listening to 3000 motherfucker');
-
-
-var http = require('http');
-var fs = require('fs');
-
-var server = http.createServer(function (req,res)
-{
-        console.log('request was made: '+ req.url);
-        if(req.url ==='/home'|| req.url ==='/')
-        {
-            res.writeHead(200,{'Content-Type':'text/html'})
-            fs.createReadStream(__dirname+'/index.html').pipe(res);
-        }else if(req.url==='/api/ninjas')
-        {
-            var ninjas = [{name:'ryu',age:30},{name:'mitch',age:10}];
-            res.writeHead(200,{'Content-Type':'application/json'});
-            res.end(JSON.stringify(ninjas));
-        }
-});
-
-server.listen(3000,'127.0.0.1');
-
-console.log('listening to port 3000 motherfucker');
-    EXPRESS
-    var express = require('express');
-
-var app = express();
-
-app.get('/',function(req,res)
-{
-    res.send('this is homepage');
-});
-
-app.get('/contact',function(req,res)
-{
-    res.send('this is contact');
-});
-
-app.listen(3000);
-
- */
+// express application
 var express = require('express');
+// require the controller we make
+var surveyController = require('./surveyController');
 
 var app = express();
 
-app.get('/',function(req,res)
-{
-    res.send('this is homepage');
-});
+// set up template engine
+app.set('view engine', 'ejs');
 
-app.get('/contact',function(req,res)
-{
-    res.send('this is contact');
-});
+// static file serving
+app.use(express.static('./public'));
 
-app.get('/profile/:id',function(req,res)
-{
-    res.send('this is contact');
-});
+// fire function from surveyController
+surveyController(app);
 
+// listen to port
 app.listen(3000);
+console.log('listening port 3000');
